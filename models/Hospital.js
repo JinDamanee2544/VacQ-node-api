dc570e5d4e6d2771d64d91deb104021b446c3e46
JinDamanee <88868930+JinDamanee2544@users.noreply.github.com>
2023-02-06 05:48:36 +0700

    feat: connect mongodb server

---
+++ models/Hospital.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const HospitalScheme = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please add a name"],
+    unique: true,
+    trim: true,
+    maxlength: [50, "Name can not be more than 50 characters"],
+  },
+  address: {
+    type: String,
+    required: [true, "Please add an address"],
+  },
+  district: {
+    type: String,
+    required: [true, "Please add a district"],
+  },
+  province: {
+    type: String,
+    required: [true, "Please add a province"],
+  },
+  postalcode: {
+    type: String,
+    required: [true, "Please add a postal code"],
+  },
+  tel: {
+    type: String,
+  },
+  region: {
+    type: String,
+    required: [true, "Please add a region"],
+  },
+});
+
+module.exports = mongoose.model("Hospital", HospitalScheme);
