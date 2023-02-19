c06cd99f2061d34d451ea45f25a4d19b1f1cd2ba
Punnapob Jindakul <jindamaneee@ManeeBook.local>
2023-02-19 16:30:24 +0700

    feat: auth model

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const hospital = require("./routes/hospitals");
+const auth = require("./routes/auth");
 const connectDB = require("./config/db");
 dotenv.config({ path: "./config/config.env" });
 
 connectDB();
 const app = express();
 app.use(express.json());
-app.use("/api/v1/hospitals", hospital);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.use("/api/v1/hospitals", hospital);
+app.use("/api/v1/auth", auth);
+
+const PORT = process.env.PORT || 8080;
+const server = app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
 
