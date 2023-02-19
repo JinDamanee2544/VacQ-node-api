1af92dac94e94109d26fd4b51f4e1a971dc99a45
Punnapob Jindakul <jindamaneee@ManeeBook.local>
2023-02-19 18:21:00 +0700

    feat: add login/register

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,72 @@
-const user = require("../models/User");
+const User = require("../models/User");
 
 // @desc      Register user
 // @route     POST /api/v1/auth/register
 // @access    Public
-exports.register = (req, res, next) => {
-  res.status(200).json({ success: true });
+exports.register = async (req, res, next) => {
+  try {
+    const { name, email, password, role } = req.body;
+    const user = await User.create({
+      name,
+      email,
+      password,
+      role,
+    });
+    // const token = user.getSignedJwtToken();
+    // res.status(201).json({ success: true, token: token });
+    sendTokenResponse(user, 201, res);
+  } catch (err) {
+    res.status(400).json({ success: false });
+    console.log(err.stack);
+  }
+};
+
+// @desc      Login user
+// @route     POST /api/v1/auth/login
+// @access    Public
+exports.login = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res
+      .status(400)
+      .json({ success: false, error: "Please provide an email and password" });
+  }
+
+  const user = await User.findOne({ email }).select("+password");
+  if (!user) {
+    return res
+      .status(401)
+      .json({ success: false, error: "Invalid credentials" });
+  }
+
+  const isMatch = await user.matchPassword(password);
+  if (!isMatch) {
+    return res
+      .status(401)
+      .json({ success: false, error: "Invalid credentials" });
+  }
+
+  // const token = user.getSignedJwtToken();
+  // res.status(200).json({ success: true, token: token });
+  sendTokenResponse(user, 201, res);
+};
+
+const sendTokenResponse = (user, statusCode, res) => {
+  const token = user.getSignedJwtToken();
+
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000 // in milliseconds
+    ),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === "production") {
+    options.secure = true;
+  }
+
+  res.status(statusCode).cookie("token", token, options).json({
+    success: true,
+    token,
+  });
 };
--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+
+dotenv.config({ path: "./config/config.env" });
+
 const hospital = require("./routes/hospitals");
 const auth = require("./routes/auth");
 const connectDB = require("./config/db");
-dotenv.config({ path: "./config/config.env" });
 
 connectDB();
+
 const app = express();
+
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/v1/hospitals", hospital);
 app.use("/api/v1/auth", auth);
